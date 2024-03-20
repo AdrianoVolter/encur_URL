020477e3a2de7df7cb46bfa1662763445393b1e5
adrianovolter <adriano.j.volter@hotmail.com>
2024-03-19 21:38:53 -0300

    arquivos do bancos/ criando metodo post

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import Fastify from 'fastify';
+import { z } from 'zod';
+import { sql } from './lib/postgres';
 const app = Fastify();
 
 
-app.get('/', async (request, reply) => {
-  return { hello: 'world' };
-  
+app.post('/api/links', async (request, reply) => {
+    const createLinkSchema = z.object({
+        code: z.string().min(3),
+        url: z.string().url(),
+    });
+  const {code , url} = createLinkSchema.parse(request.body);
+
+  const result = await sql /*sql*/`
+  INSERT INTO short_links (code, original_url) 
+  VALUES (${code}, ${url}) 
+  RETURNING id`
+
+  const link = result[0];
+
+  return reply.code(201).send({ shortLinkID: link.id });
 });
 
 app.listen({
--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,15 @@
 import { sql } from  "./lib/postgres"
 
 async function setup() {
-    
-    }
+   await sql/*sql*/`CREATE TABLE IF NOT EXISTS short_links(
+    id SERIAL PRIMARY KEY,
+    code TEXT UNIQUE ,
+    original_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   )`
+
+   await sql.end()
+    console.log('Database setup complete')
+}
+
+setup()
