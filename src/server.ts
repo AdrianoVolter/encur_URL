245a09628bef4cd23daba94a667bb2f70563c84c
adrianovolter <adriano.j.volter@hotmail.com>
2024-03-19 22:23:53 -0300

    criando metodo get

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,50 @@
-import Fastify from 'fastify';
-import { z } from 'zod';
-import { sql } from './lib/postgres';
+import Fastify from "fastify";
+import { z } from "zod";
+import { sql } from "./lib/postgres";
+import postgres from "postgres";
 const app = Fastify();
 
+app.get("/api/links", async (request, reply) => {
+  const result = await sql/*sql*/ `
+    SELECT *
+    FROM short_links
+    ORDER BY created_at DESC`;
 
-app.post('/api/links', async (request, reply) => {
-    const createLinkSchema = z.object({
-        code: z.string().min(3),
-        url: z.string().url(),
-    });
-  const {code , url} = createLinkSchema.parse(request.body);
-
-  const result = await sql /*sql*/`
-  INSERT INTO short_links (code, original_url) 
-  VALUES (${code}, ${url}) 
-  RETURNING id`
-
-  const link = result[0];
-
-  return reply.code(201).send({ shortLinkID: link.id });
+  return result
 });
 
-app.listen({
+app.post("/api/links", async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  });
+  const { code, url } = createLinkSchema.parse(request.body);
+
+  try {
+    const result = await sql/*sql*/ `
+  INSERT INTO short_links (code, original_url) 
+  VALUES (${code}, ${url}) 
+  RETURNING id`;
+
+    const link = result[0];
+
+    return reply.code(201).send({ shortLinkID: link.id });
+  } catch (err) {
+    if (err instanceof postgres.PostgresError) {
+      if (err.code === "23505") {
+        return reply.code(400).send({ message: "Code duplication error" });
+      }
+    }
+
+    console.error(err);
+    return reply.code(500).send({ message: "Internal server error" });
+  }
+});
+
+app
+  .listen({
     port: 3000,
-}).then(()=>{
-    console.log('Server started at port 3000');
-})
+  })
+  .then(() => {
+    console.log("Server started at port 3000");
+  });
